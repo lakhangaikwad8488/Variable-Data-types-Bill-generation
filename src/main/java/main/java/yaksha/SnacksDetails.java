3f785d9b2353caabb6fbe6e1f4b72d00da3c7973
lakhangaikwad8488 <70312128+lakhangaikwad8488@users.noreply.github.com>
2020-10-16 12:47:15 +0530

    Update SnacksDetails.java

--- src/main/java/main/java/yaksha/SnacksDetails.java
+++ src/main/java/main/java/yaksha/SnacksDetails.java
@@ -3,7 +3,8 @@ package main.java.yaksha;
 public class SnacksDetails {
 	 public double generateBill(int pizza, int puff, int drink) {
 		
-      /* You code here*/
+      /* You code here
+      Method should get no. Of pizza, no. Of puff and no. Of drink from BillGeneratorMain and using this values find total bill. This method should return bill*/
 		 return 0;
 	}
 
